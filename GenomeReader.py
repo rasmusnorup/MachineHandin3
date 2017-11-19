f21efef9df5688562568d93211e8534d15f6a48e
Rasmus <norup.rasmus@gmail.com>
2017-11-19 13:14:29 +0100

    Initial commit

---
+++ GenomeReader.py
@@ -0,0 +1,105 @@
+class hmm:
+    def __init__(self, init_probs, trans_probs, emission_probs):
+        self.init_probs = init_probs
+        self.trans_probs = trans_probs
+        self.emission_probs = emission_probs
+
+
+
+init_probs_7_state = [0.00, 0.00, 0.00, 1.00, 0.00, 0.00, 0.00]
+
+trans_probs_7_state = [
+    [0.00, 0.00, 0.90, 0.10, 0.00, 0.00, 0.00],
+    [1.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
+    [0.00, 1.00, 0.00, 0.00, 0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.05, 0.90, 0.05, 0.00, 0.00],
+    [0.00, 0.00, 0.00, 0.00, 0.00, 1.00, 0.00],
+    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 1.00],
+    [0.00, 0.00, 0.00, 0.10, 0.90, 0.00, 0.00],
+]
+
+emission_probs_7_state = [
+    #   A     C     G     T
+    [0.30, 0.25, 0.25, 0.20],
+    [0.20, 0.35, 0.15, 0.30],
+    [0.40, 0.15, 0.20, 0.25],
+    [0.25, 0.25, 0.25, 0.25],
+    [0.20, 0.40, 0.30, 0.10],
+    [0.30, 0.20, 0.30, 0.20],
+    [0.15, 0.30, 0.20, 0.35],
+]
+
+hmm_7_state = hmm(init_probs_7_state, trans_probs_7_state, emission_probs_7_state)
+
+
+
+
+def convertAnnToState(ann):
+    state = {}
+    cCount = 2
+    rCount = 4
+    for i in range(0,ann.len()):
+        if ann[i] == "N":
+            state[i] = 3
+        if ann[i] == "C":
+            state[i] = cCount
+            cCount -= 1
+            if cCount==-1:
+                cCount = 2
+        if ann[i] == "R":
+            state[i] = rCount
+            rCount += 1
+            if rCount == 7:
+                rCount = 4
+    return state
+
+
+
+
+
+def translate_indices_to_observations(indices):
+    mapping = ['a', 'c', 'g', 't']
+    return ''.join(mapping[idx] for idx in indices)
+
+def translate_path_to_indices(path):
+    return list(map(lambda x: int(x), path))
+
+def translate_indices_to_path(indices):
+    return ''.join([str(i) for i in indices])
+
+def make_table(m, n):
+    """Make a table with `m` rows and `n` columns filled with zeros."""
+    return [[0] * n for _ in range(m)]
+
+def compute_accuracy(true_ann, pred_ann):
+    if len(true_ann) != len(pred_ann):
+        return 0.0
+    return sum(1 if true_ann[i] == pred_ann[i] else 0
+               for i in range(len(true_ann))) / len(true_ann)
+
+def read_fasta_file(filename):
+    """
+    Reads the given FASTA file f and returns a dictionary of sequences.
+
+    Lines starting with ';' in the FASTA file are ignored.
+    """
+    sequences_lines = {}
+    current_sequence_lines = None
+    with open(filename) as fp:
+        for line in fp:
+            line = line.strip()
+            if line.startswith(';') or not line:
+                continue
+            if line.startswith('>'):
+                sequence_name = line.lstrip('>')
+                current_sequence_lines = []
+                sequences_lines[sequence_name] = current_sequence_lines
+            else:
+                if current_sequence_lines is not None:
+                    current_sequence_lines.append(line)
+    sequences = {}
+    for name, lines in sequences_lines.items():
+        sequences[name] = ''.join(lines)
+    return sequences
+
+print(convertAnnToState(read_fasta_file("true-ann1.fa")))
